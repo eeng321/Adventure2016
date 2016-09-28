c9ee3fbe0ca9aa335bbc3f72a8260a14f74f8cbd
eeng <eeng@sfu.ca>
2016-09-28 15:38:54 -0700

    crud operations

--- src/player/playerDriver.cxx
+++ src/player/playerDriver.cxx
@@ -7,9 +7,9 @@
 #include <odb/database.hxx>
 #include <odb/transaction.hxx>
 
-#include <odb/mysql/database.hxx>
+#include <odb/sqlite/database.hxx>
 
-#include "person.hxx"
+#include "player.hxx"
 #include "player-odb.hxx"
 
 using namespace std;
@@ -50,29 +50,89 @@ using namespace odb::core;
 // }
 
 //create entry into db
-int Create(string const name){
+int Create(string const playerName){
   try{
-    player username (name, 100); //create player object
+    player user (playerName, 100); //create player object
     transaction t (db->begin());
-    db->persist(username); //make objects persistent
+
+    db->persist(user); //make objects persistent
+
     t.commit();
   }
   catch (const odb::exception& e){
     cerr << e.what () << endl;
-    return 1;
+    return 1;  
   }
 
 }
 
-int Retrieve(int argc, char* argv[]){
+//player object names should be unique
+//returns player id
+int Retrieve(string const playerName){
 
+  try{
+    typedef odb::query<player> query;
+    typedef odb::result<player> result;
+    transaction t (db->begin());
+
+    result r (db->query<player> (query::name == playerName); //query database for matching names
+
+    for (result::iterator i (r.begin ()); i != r.end (); ++i)
+    {
+      int playerID = i->id; //return first match's id b/c there should only be 1 match
+      break; //leave loop after first match, incase there is multiple due to error
+    }
+
+    t.commit ();
+
+    return playerID; //return target player id
+  }
+  catch (const odb::exception& e)
+  {
+    cerr << e.what () << endl;
+    return -1; //return something not in db
+  }
+  
 }
 
-int Update(int argc, char* argv[]){
+//add stuff into param for changing
+int Update(string const playerName){
+  int playerID = Retrieve(playerName); //get id of the player name you want
 
+  if(playerID != -1){ //if we can find player in db
+    try{
+      transaction t (db->begin ());
+
+      auto_ptr<player> user (db->load<player> (playerID));
+      //update changes
+      //.
+      //.
+      db->update (*user);
+
+      t.commit ();
+    }
+    catch (const odb::exception& e)
+    {
+      cerr << e.what () << endl;
+      return 1;
+    }
+  }
+  return 0; //failed
 }
 
-int Delete(int argc, char* argv[]){
+//delete player from the database
+void Delete(string const playerName){
+  int playerID = Retrieve(playerName);
+
+  if(playerID != -1){ //if player is found
+    transaction t (db->begin ());
+
+    auto_ptr<player> user (db->load<player> (playerID)); //grab player
+
+    db->erase (*user);
+
+    t.commit ();
+  }
 
 }
 
