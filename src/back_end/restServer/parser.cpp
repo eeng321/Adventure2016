a6b3e31048c3ce13556d500e04743de7f69a3d3c
wbong@sfu.ca <wbong>
2016-10-10 20:24:59 -0700

    moved dummy to adventure2016

--- src/back_end/restServer/parser.cpp
+++ src/back_end/restServer/parser.cpp
@@ -1,27 +1,19 @@
-//
-// Created by william on 27/09/16.
-//
 
-#include "yaml-cpp/yaml.h"
+#include "../lib/yaml-parser/include/yaml-cpp/yaml.h"
+
 #include <iostream>
 #include <fstream>
-// #include ""
 
 using namespace std;
 
 int main(int argc, char** argv) {
     // std::ifstream fin("/home/william/cmpt373/adventure2016/src/yaml-parser/smurf.yaml");
     // std::cout << fin.rdbuf();
-    YAML::Node node = YAML::LoadFile("/home/william/cmpt373/adventure2016/src/back_end/lib/yaml-parser/smurf.yaml");
+
+    YAML::Node node = YAML::LoadFile();
     std::cout << "test\n";
     YAML::Node roomNode = node["ROOMS"];//desc, doors(), extended_descriptions, id, name
 
-    for(auto i : roomNode){
-    	std::cout << i["id"] << "\n";
-    }
     return 0;
 }
-
-
-
 
