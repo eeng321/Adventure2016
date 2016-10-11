a70ccd4ee0d5abd9404cfa4f17dee33e6d85621a
wbong@sfu.ca <wbong>
2016-10-11 15:58:35 -0700

    linked yaml-parser and deleted test directory which gave warnings

--- src/back_end/restServer/parser.cpp
+++ src/back_end/restServer/parser.cpp
@@ -1,19 +1,20 @@
 
-#include "../lib/yaml-parser/include/yaml-cpp/yaml.h"
-
+#include "yaml-cpp/yaml.h"
 #include <iostream>
 #include <fstream>
 
 using namespace std;
-
 int main(int argc, char** argv) {
+
+
     // std::ifstream fin("/home/william/cmpt373/adventure2016/src/yaml-parser/smurf.yaml");
     // std::cout << fin.rdbuf();
-
-    YAML::Node node = YAML::LoadFile();
+    YAML::Node node = YAML::LoadFile("/home/william/cmpt373/adventure2016/src/back_end/lib/yaml-parser/smurf.yaml");
     std::cout << "test\n";
-    YAML::Node roomNode = node["ROOMS"];//desc, doors(), extended_descriptions, id, name
-
+//    YAML::Node roomNode = node["ROOMS"];//desc, doors(), extended_descriptions, id, name
+//
+//    for(auto i : roomNode){
+//    	std::cout << i["id"] << "\n";
+//    }
     return 0;
 }
-
