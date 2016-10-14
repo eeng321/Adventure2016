df319b48ba1de212b6a80ed55c0ccde3d181914f
wbong@sfu.ca <wbong>
2016-10-14 09:50:26 -0700

    isolated parser class for testing

--- src/back_end/restServer/parser.cpp
+++ src/back_end/restServer/parser.cpp
@@ -6,19 +6,35 @@
 using namespace std;
 using namespace parser;
 
+int main(){
+    std::string test = "test";
+    std::string test2 = "test2";
+
+    YAML::Node nodeTest = parser::serializeLogin(test, test2);
+    cout << "test: " << nodeTest["USERNAME"] << endl;
+
+    string test3;
+    string test4;
+
+    parser::deserializeLogin(test3, test4, nodeTest);
+
+    cout << "Test 3 and 4: " << test3 << " " << test4 << endl;
+    return 0;
+}
+
 
 YAML::Node parser::serializeLogin(const std::string username, const std::string password){
 
-    YAML::Node node = YAML::LoadFile("yaml.yaml");
-    //std::cout << node["USERNAME"];
-    //node["PASSWORD"] = password;
+    YAML::Node node;
+    node["USERNAME"] = username;
+    node["PASSWORD"] = password;
     return node;
 }
 
 void parser::deserializeLogin(std::string &username, std::string &password, const YAML::Node node){
     
-     //node["USERNAME"] >> username;
-    //password = node["PASSWORD"].as<string>();
+    username = node["USERNAME"].as<string>();
+    password = node["PASSWORD"].as<string>();
 }
 
 
