3b58a04ba1bdadeef05862a6222e0c29c0db4a05
wbong@sfu.ca <wbong>
2016-10-14 09:15:28 -0700

    debug old functions

--- src/back_end/restServer/parser.cpp
+++ src/back_end/restServer/parser.cpp
@@ -10,14 +10,14 @@ using namespace parser;
 YAML::Node parser::serializeLogin(const std::string username, const std::string password){
 
     YAML::Node node = YAML::LoadFile("yaml.yaml");
-    //std::cout << node["USERNAME"];
+    std::cout << node["USERNAME"];
     //node["PASSWORD"] = password;
     return node;
 }
 
 void parser::deserializeLogin(std::string &username, std::string &password, const YAML::Node node){
     
-     node["USERNAME"] >> username;
+     //node["USERNAME"] >> username;
     //password = node["PASSWORD"].as<string>();
 }
 
