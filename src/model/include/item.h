b92c6ff98273ff15f2209d7eb7aaf75aadd96c23
bha33 <bha33@sfu.ca>
2016-10-20 15:28:03 -0700

    Initial item implementation

--- src/model/include/item.h
+++ src/model/include/item.h
@@ -7,6 +7,8 @@
 
 #include<memory>
 
+using id = int;
+
 class ItemModel;
 
 class Item {
@@ -14,9 +16,32 @@ private:
 	std::unique_ptr<ItemModel> model;
 
 public:
+	Item(id itemIdIn,
+		const std::vector<std::string>& keywordsIn,
+		const std::string& longDescIn,
+		const std::string& shortDescIn,
+		const std::vector<std::string>& extra);
 
+	void build(id itemIdIn,
+		const std::vector<std::string>& keywordsIn,
+		const std::string& longDescIn,
+		const std::string& shortDescIn,
+		const std::vector<std::string>& extra);
 
-	void build();
+	/* Getters */
+	id getId();
+	std::vector<std::string> getKeywords();
+	std::string getlongDesc();
+	std::string getShortDesc();
+	std::vector<std::string> getExtra();
+
+	/* Setters */
+	void setId(id idIn);
+	void setKeywords(const std::vector<std::string>& keywordsIn);
+	void setlongDesc(const std::string& longDescIn);
+	void setShortDesc(const std::string& shortDescIn);
+	void setExtra(const std::vector<std::string>& extraIn);
+
 };
 
 
--- src/model/lib/item/item.cpp
+++ src/model/lib/item/item.cpp
@@ -10,7 +10,66 @@
 using std::string;
 using std::vector;
 
-void Item::build() {
+Item::Item(id itemIdIn,
+	const std::vector<std::string>& keywordsIn,
+	const std::string& longDescIn,
+	const std::string& shortDescIn,
+	const std::vector<std::string>& extraIn)
+	:model(std::make_unique<ItemModel>()){
+
+	build(itemIdIn, keywordsIn, longDescIn, shortDescIn, extraIn);
+}
+
+void Item::build(id itemIdIn,
+	const std::vector<std::string>& keywordsIn,
+	const std::string& longDescIn,
+	const std::string& shortDescIn,
+	const std::vector<std::string>& extraIn){
+
+	model->itemId = itemIdIn;
+	model->keywords = keywordsIn;
+	model->longdesc = longDescIn;
+	model->shortdesc = shortDescIn;
+	model->extra = extraIn;
 
 }
 
+id Item::getId(){
+	return model->itemId;
+}
+
+std::vector<std::string> Item::getKeywords(){
+	return model->keywords;
+}
+
+std::string Item::getlongDesc(){
+	return model->longdesc;
+}
+
+std::string Item::getShortDesc(){
+	return model->shortdesc;
+}
+
+std::vector<std::string> Item::getExtra(){
+	return model->extra;
+}
+
+/* Setters */
+void Item::setId(id idIn){
+	model->itemId = idIn;
+}
+void Item::setKeywords(const std::vector<std::string>& keywordsIn){
+	model->keywords = keywordsIn;
+}
+
+void Item::setlongDesc(const std::string& longDescIn){
+	model->longdesc = longDescIn;
+}
+
+void Item::setShortDesc(const std::string& shortDescIn){
+	model->shortdesc = shortDescIn;
+}
+
+void Item::setExtra(const std::vector<std::string>& extraIn){
+	model->extra = extraIn;
+}
