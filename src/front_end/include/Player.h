2d2762e4d8a79da63328f2772aa39a330214b266
bha33 <bha33@sfu.ca>
2016-11-05 12:58:42 -0700

    Changed tabs to spaces

--- src/front_end/include/Player.h
+++ src/front_end/include/Player.h
@@ -11,73 +11,73 @@ class PlayerModel;
 
 class Player {
 public:
-	Player(playerId idIn);
+    Player(playerId idIn);
 
-	Player(playerId idIn,
-		   std::string username,
-		   roomId locationIn,
-		   std::string damageIn,
-		   int healthIn,
-		   int armorIn,
-		   std::string hitIn,
-		   int expIn,
-		   int goldIn,
-		   int levelIn,
-		   int thac0In,
-		   std::vector<itemId> inventoryIn);
+    Player(playerId idIn,
+           std::string username,
+           roomId locationIn,
+           std::string damageIn,
+           int healthIn,
+           int armorIn,
+           std::string hitIn,
+           int expIn,
+           int goldIn,
+           int levelIn,
+           int thac0In,
+           std::vector<itemId> inventoryIn);
 
-	PlayerModel getModel();
+    PlayerModel getModel();
 
-	void setModel(PlayerModel model);
+    void setModel(PlayerModel model);
 
-	playerId getId() const;//used to be get getPlayerId
-	std::string getUsername() const;
+    playerId getId() const;//used to be get getPlayerId
+    std::string getUsername() const;
 
-	roomId getLocation() const;//used to be getRoomId
-	int getHealth() const;
+    roomId getLocation() const;//used to be getRoomId
+    int getHealth() const;
 
-	std::string getDamage() const;
+    std::string getDamage() const;
 
-	int getArmor() const;
+    int getArmor() const;
 
-	std::string getHit() const;
+    std::string getHit() const;
 
-	int getExp() const;
+    int getExp() const;
 
-	int getGold() const;
+    int getGold() const;
 
-	int getLevel() const;
+    int getLevel() const;
 
-	int getThac0() const;
+    int getThac0() const;
 
-	std::vector<itemId> getInventory() const;
+    std::vector<itemId> getInventory() const;
 
-	void incrementLevel();
+    void incrementLevel();
 
-	void addExp(int expAdded);
+    void addExp(int expAdded);
 
-	void setLocation(roomId id);;//used to be updateRoomId
-	void deceaseHealth(int change);
+    void setLocation(roomId id);;//used to be updateRoomId
+    void deceaseHealth(int change);
 
-	void increaseHealth(int change);
+    void increaseHealth(int change);
 
-	void addToInventory(itemId item);
+    void addToInventory(itemId item);
 
-	void removeFromInventory(const itemId &item);
+    void removeFromInventory(const itemId &item);
 
 private:
-	playerId id;
-	std::string username;//used to be loginName
-	roomId location;
-	int health;
-	std::string damage;
-	int armor;
-	std::string hit;
-	int exp;
-	int gold;
-	int level;
-	int thac0;
-	std::vector<itemId> inventory;
+    playerId id;
+    std::string username;//used to be loginName
+    roomId location;
+    int health;
+    std::string damage;
+    int armor;
+    std::string hit;
+    int exp;
+    int gold;
+    int level;
+    int thac0;
+    std::vector<itemId> inventory;
 };
 
 #endif //ADVENTURE2016_PLAYER_H
--- src/front_end/lib/player/Player.cpp
+++ src/front_end/lib/player/Player.cpp
@@ -16,154 +16,154 @@ const int DEFAULT_SPAWN = 0;
 const int START_HEALTH = 100;
 
 Player::Player(playerId idIn)
-		: id(std::move(idIn)),
-		  username(""),
-		  location(DEFAULT_SPAWN),
-		  health(START_HEALTH) {}
+        : id(std::move(idIn)),
+          username(""),
+          location(DEFAULT_SPAWN),
+          health(START_HEALTH) {}
 
 Player::Player(playerId idIn,
-			   string usernameIn,
-			   roomId locationIn,
-			   string damageIn,
-			   int healthIn,
-			   int armorIn,
-			   string hitIn,
-			   int expIn,
-			   int goldIn,
-			   int levelIn,
-			   int thac0In,
-			   std::vector<itemId> inventoryIn)
-		: id(std::move(idIn)),
-		  username(std::move(usernameIn)),
-		  location(std::move(locationIn)),
-		  damage(std::move(damageIn)),
-		  armor(armorIn),
-		  hit(std::move(hitIn)),
-		  exp(expIn),
-		  gold(goldIn),
-		  level(levelIn),
-		  thac0(thac0In),
-		  health(healthIn),
-		  inventory(std::move(inventoryIn)) {}
+               string usernameIn,
+               roomId locationIn,
+               string damageIn,
+               int healthIn,
+               int armorIn,
+               string hitIn,
+               int expIn,
+               int goldIn,
+               int levelIn,
+               int thac0In,
+               std::vector<itemId> inventoryIn)
+        : id(std::move(idIn)),
+          username(std::move(usernameIn)),
+          location(std::move(locationIn)),
+          damage(std::move(damageIn)),
+          armor(armorIn),
+          hit(std::move(hitIn)),
+          exp(expIn),
+          gold(goldIn),
+          level(levelIn),
+          thac0(thac0In),
+          health(healthIn),
+          inventory(std::move(inventoryIn)) {}
 
 PlayerModel Player::getModel() {
-	PlayerModel model;
-	model.playerId = id.value;
-	model.loginName = username;
-	model.roomId = location.value;
-	model.damage = damage;
-	model.armor = armor;
-	model.hit = hit;
-	model.exp = exp;
-	model.gold = gold;
-	model.level = level;
-	model.thac0 = thac0;
-	model.health = health;
-	for (const itemId &item: inventory) {
-		model.inventory.push_back(item.value);
-	}
-	return model;
+    PlayerModel model;
+    model.playerId = id.value;
+    model.loginName = username;
+    model.roomId = location.value;
+    model.damage = damage;
+    model.armor = armor;
+    model.hit = hit;
+    model.exp = exp;
+    model.gold = gold;
+    model.level = level;
+    model.thac0 = thac0;
+    model.health = health;
+    for (const itemId &item: inventory) {
+        model.inventory.push_back(item.value);
+    }
+    return model;
 }
 
 void Player::setModel(PlayerModel model) {
-	id.value = model.playerId;
-	username = model.loginName;
-	location = model.roomId;
-	damage = model.damage;
-	armor = model.armor;
-	hit = model.hit;
-	exp = model.exp;
-	gold = model.gold;
-	level = model.level;
-	thac0 = model.thac0;
-	health = model.health;
+    id.value = model.playerId;
+    username = model.loginName;
+    location = model.roomId;
+    damage = model.damage;
+    armor = model.armor;
+    hit = model.hit;
+    exp = model.exp;
+    gold = model.gold;
+    level = model.level;
+    thac0 = model.thac0;
+    health = model.health;
 
-	inventory.clear();
-	for (int item : model.inventory) {
-		inventory.push_back(itemId(item));
-	}
+    inventory.clear();
+    for (int item : model.inventory) {
+        inventory.push_back(itemId(item));
+    }
 }
 
 playerId Player::getId() const {
-	return id;
+    return id;
 }
 
 string Player::getUsername() const {
-	return username;
+    return username;
 }
 
 roomId Player::getLocation() const {
-	return location;
+    return location;
 }
 
 string Player::getDamage() const {
-	return damage;
+    return damage;
 }
 
 int Player::getArmor() const {
-	return armor;
+    return armor;
 }
 
 string Player::getHit() const {
-	return hit;
+    return hit;
 }
 
 int Player::getExp() const {
-	return exp;
+    return exp;
 }
 
 int Player::getGold() const {
-	return gold;
+    return gold;
 }
 
 int Player::getLevel() const {
-	return level;
+    return level;
 }
 
 int Player::getThac0() const {
-	return thac0;
+    return thac0;
 }
 
 std::vector<itemId> Player::getInventory() const {
-	return inventory;
+    return inventory;
 }
 
 int Player::getHealth() const {
-	return health;
+    return health;
 }
 
 void Player::addToInventory(itemId item) {
-	inventory.push_back(std::move(item));
+    inventory.push_back(std::move(item));
 }
 
 void Player::removeFromInventory(const itemId &item) {
-	auto element = std::find(inventory.begin(), inventory.end(), item);
-	if (element == inventory.end()) {
-		throw std::domain_error("item not in inventory");
-	}
-	inventory.erase(element);
+    auto element = std::find(inventory.begin(), inventory.end(), item);
+    if (element == inventory.end()) {
+        throw std::domain_error("item not in inventory");
+    }
+    inventory.erase(element);
 }
 
 void Player::incrementLevel() {
-	level++;
+    level++;
 }
 
 void Player::addExp(int expAdded) {
-	exp = exp + expAdded;
-	if (exp > 100) {
-		exp = exp - 100;
-		incrementLevel();
-	}
+    exp = exp + expAdded;
+    if (exp > 100) {
+        exp = exp - 100;
+        incrementLevel();
+    }
 }
 
 void Player::deceaseHealth(int change) {
-	health = health - change;
+    health = health - change;
 }
 
 void Player::increaseHealth(int change) {
-	health = health + change;
+    health = health + change;
 }
 
 void Player::setLocation(roomId id) {
-	location = id;
+    location = id;
 }
