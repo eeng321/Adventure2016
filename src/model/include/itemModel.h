d111622581bd7621fbbca24b4d1fe0e537889a51
lhliu <lhliu@sfu.ca>
2016-11-06 12:53:41 -0800

    removed undefined itemModel constructor

--- src/model/include/itemModel.h
+++ src/model/include/itemModel.h
@@ -16,7 +16,16 @@
 
 class ItemModel {
 public:
-<<<<<<< d43d422b6489c421974d78511a8c5c2b68c1d44a
+    friend class hiberlite::access;
+    template<class Archive>
+    void hibernate(Archive & ar) {
+        ar & HIBERLITE_NVP(extra);
+        ar & HIBERLITE_NVP(id);
+        ar & HIBERLITE_NVP(keywords);
+        ar & HIBERLITE_NVP(longDesc);
+        ar & HIBERLITE_NVP(shortDesc);
+    }
+
     std::vector<std::string> attributes;
     int cost;
     std::vector<std::string> extra;
@@ -28,25 +37,8 @@ public:
     std::vector<std::string> wear_flags;
     int weight;
 
-    ItemModel();
-=======
-	friend class hiberlite::access;
-    template<class Archive>
-    void hibernate(Archive & ar) {
-        ar & HIBERLITE_NVP(extra);
-        ar & HIBERLITE_NVP(id);
-        ar & HIBERLITE_NVP(keywords);
-        ar & HIBERLITE_NVP(longDesc);
-        ar & HIBERLITE_NVP(shortDesc);
-    }
-
-	std::vector<std::string> extra;
-	int id;
-	std::vector<std::string> keywords;
-	std::string longDesc;
-	std::string shortDesc;
 	ItemModel();
->>>>>>> fixed general typos and bugs. Updated itemModel to contain hiberlite::access friend class.
+
 };
 
 
