677b1a9314edb1168a6944371d052e3293dd6e32
bha33 <bha33@sfu.ca>
2016-11-15 17:54:41 -0800

    Initial piglatin implementation

---
+++ src/front_end/include/piglatin.h
@@ -0,0 +1,14 @@
+//
+// Created by braden on 14/11/16.
+//
+
+#ifndef ADVENTURE2016_PIGLATIN_H
+#define ADVENTURE2016_PIGLATIN_H
+
+#include <string>
+
+std::string translateToPiglatin(const std::string& s);
+int findVowel(const std::string& s);
+std::string translateWord(const std::string& s);
+
+#endif //ADVENTURE2016_PIGLATIN_H
